3fe982b45d9c284260b370e0adbce57ce86d5910
irradev <israeldevcorel@gmail.com>
2025-12-18 05:33:35 -0600

    feat: Configure Netlify deployment with web redirects and integrate TMDB API token from environment variables.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
+import 'config/constants/environment.dart';
 import 'config/database/tarsier/tasier_database.dart';
 import 'config/router/app_router.dart';
 import 'config/theme/app_theme.dart';
@@ -12,6 +13,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: ".env");
+
+  debugPrint('TMDB TOKEN: ${Environment.tmdbApiToken}');
 
   if (kIsWeb) {
     usePathUrlStrategy();
